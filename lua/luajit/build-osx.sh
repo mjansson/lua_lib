a139ddcd0b47fe75df5c98ef402e2e0aa93b011f
Mattias Jansson <mattias@rampantpixels.com>
2016-07-11 19:05:31 +0200

    simplify mac build to 64 bit only

--- lua/luajit/build-osx.sh
+++ lua/luajit/build-osx.sh
@@ -4,10 +4,6 @@ rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 rm *.a 1>/dev/null 2>/dev/null
 
-#make CC="gcc -m32" clean
-#make CC="gcc -m32" amalg
-#mv src/libluajit.a $BUILD_DIR/libluajit32.a
-
 make clean
 make amalg
 mv src/libluajit.a $BUILD_DIR/libluajit64.a
