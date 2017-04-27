1fa1ccb5599a767566903f33143984eff24616d4
Mattias Jansson <mattias@rampantpixels.com>
2017-04-27 12:18:24 +0200

    update macos build script

--- lua/luajit/build-macos.sh
+++ lua/luajit/build-macos.sh
@@ -9,10 +9,10 @@ make amalg
 mv src/libluajit.a $BUILD_DIR/libluajit64.a
 
 #libtool -o ../../lib/macosx/libluajit.a $BUILD_DIR/libluajit32.a $BUILD_DIR/libluajit64.a 2> /dev/null
-cp $BUILD_DIR/libluajit64.a ../../lib/macosx/libluajit.a
+cp $BUILD_DIR/libluajit64.a ../../lib/macos/libluajit.a
 
 make clean
 make amalg LUAJIT_ENABLE_GC64=1 LUAJIT_DISABLE_JIT=1
 mv src/libluajit.a $BUILD_DIR/libluajitgc64.a
 
-cp $BUILD_DIR/libluajitgc64.a ../../lib/macosx/libluajit-gc64.a
+cp $BUILD_DIR/libluajitgc64.a ../../lib/macos/libluajit-gc64.a
