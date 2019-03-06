763d84361fde6694acba1c9aa12f176906b70734
Mattias Jansson <majanss@microsoft.com>
2019-03-06 14:10:14 +0100

    copy luajit for contained projects

--- lua/luajit/build-windows.bat
+++ lua/luajit/build-windows.bat
@@ -3,8 +3,10 @@ call D:\dev\vs2017\vc\Auxiliary\Build\vcvarsall.bat amd64
 cd src
 call msvcbuild.bat gc64 static
 copy /Y luajit.lib ..\..\..\lib\windows\x86-64\luajit.lib
+copy /Y luajit.lib ..\..\..\..\..\..\lib\windows\luajit.lib
 
 call msvcbuild.bat static
 copy /Y luajit.lib ..\..\..\lib\windows\x86-64\luajit32.lib
+copy /Y luajit.lib ..\..\..\..\..\..\lib\windows\luajit32.lib
 
 cd ..
